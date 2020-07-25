64b2c6c39247ac1729cc478401d2f12ddafd2f32
Bijay Raj Paudel <bijayraj.paudel@siu.edu>
2020-07-25 03:19:05 -0500

    Added sequilize models, login try, error handler, validation module

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const api = require('./src/api');
 const swagger = require('./src/swagger')
-
+const APIError = require('./src/helpers/APIError')
 const routers = require('./src/routes/routes.index')
+const helmet = require('helmet');
+const cors = require('cors');
+const expressValidation = require('express-validation')
 
 const startServer = function (port) {
     const app = express();
@@ -18,6 +21,10 @@ const startServer = function (port) {
         extended: false
     }));
     app.use(cookieParser());
+
+    app.use(helmet());
+    app.use(cors());
+
     app.set('port', port);
     const server = http.createServer(app);
     server.listen(port, () => {
@@ -38,8 +45,43 @@ const startServer = function (port) {
     app.use('/', api);
     //Configure and use swagger
     app.use('/', swagger);
-
     require('./src/routes/routes.index')(app);
+
+    // if error is not an instanceOf APIError, convert it.
+    app.use((err, req, res, next) => {
+        if (err instanceof expressValidation.ValidationError) {
+            const unifiedErrorMessage = err.details.body
+                .map(error => error.message)
+                .join(' and ');
+            const error = new APIError(unifiedErrorMessage, err.statusCode, true);
+            return next(error);
+        } else if (!(err instanceof APIError)) {
+            const apiError = new APIError(err.message, err.status || err.statusCode, err.isPublic);
+            return next(apiError);
+        }
+        return next(err);
+    });
+
+    // catch 404 and forward to error handler
+    app.use((req, res, next) => {
+        const err = new APIError('API not found', httpStatus.NOT_FOUND);
+        return next(err);
+    });
+
+
+    app.use((
+            err,
+            req,
+            res,
+            next, // eslint-disable-line no-unused-vars
+        ) =>
+        res.status(err.status || 500).json({
+            message: err.isPublic ? err.message : httpStatus[err.status],
+            stack: process.env.NODE_ENV === 'development' ? err.stack : {},
+        }));
+
+
+
     return app;
 };
 
---
+++ src/database/sequelize.js
@@ -0,0 +1,79 @@
+const Sequelize = require('sequelize');
+const fs = require('fs');
+const path = require('path');
+const _ = require('lodash');
+const config = require('../config/config');
+
+const db = {};
+
+// const sqlite = require('sqlite3');
+// const db2 = new sqlite.Database(config.DB_FILE);
+
+// connect to postgres db
+// const sequelize = new Sequelize(
+//     // config.postgres.db,
+//     // config.postgres.user,
+//     // config.postgres.password, {
+//     //     dialect: 'postgres',
+//     //     port: config.postgres.port,
+//     //     host: config.postgres.host,
+//     // },
+//     {
+//         dialect: 'sqlite',
+//         storage: './db.sqlite'
+//     }
+// );
+
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: config.DB_FILE
+});
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
+
+const modelsDir = path.normalize(`${__dirname}/../models`);
+
+// loop through all files in models directory ignoring hidden files and this file
+fs
+    .readdirSync(modelsDir)
+    .filter(file => file.indexOf('.') !== 0 && file.indexOf('.map') === -1 && file.indexOf('.model.') > -1)
+    // import model files and save model names
+    .forEach((file) => {
+        console.info(`Loading model file ${file}`);
+
+
+        // const model = sequelize.import(path.join(modelsDir, file));
+        const model = require(path.join(modelsDir, file))
+
+        model.init(sequelize, Sequelize)
+
+        db[model.name] = model;
+    });
+
+// calling all the associate function, in order to make the association between the models
+Object.keys(db).forEach((modelName) => {
+    if (db[modelName].associate) {
+        db[modelName].associate(db);
+    }
+});
+
+// Synchronizing any model changes with database.
+sequelize.sync().then((err) => {
+    if (err) console.error('An error occured %j', err);
+    else console.info('Database synchronized');
+});
+
+// assign the sequelize variables to the db object and returning the db.
+module.exports = _.extend({
+        sequelize,
+        Sequelize,
+    },
+    db,
+);
